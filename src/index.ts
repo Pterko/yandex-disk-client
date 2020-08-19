aee74f3823e58ccc2ab985f834f8653ab09a033e
Pterko <pter96@gmail.com>
2020-08-20 01:48:20 +0300

    Linting fixes

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,9 @@ import got, { Got } from 'got';
 import YaAuth from './ya-auth';
 import YaOperations from './ya-operations';
 
-const {CookieJar} = require('tough-cookie');
-
-
+const { CookieJar } = require('tough-cookie');
 
 class YandexDiskClient {
-
   public login: string;
   public password: string;
   public phoneNumber: string | undefined;
@@ -18,34 +15,51 @@ class YandexDiskClient {
 
   public httpClient: Got;
 
-  constructor(login: string, password: string, options?: {phone: string}){
+  constructor(login: string, password: string, options?: { phone: string }) {
     this.login = login;
     this.password = password;
-    if (options?.phone){
+    if (options?.phone) {
       this.phoneNumber = options?.phone;
     }
 
     const cookieJar = new CookieJar();
     this.httpClient = got.extend({
-      headers: {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36'},
-      cookieJar});
+      headers: {
+        'User-Agent':
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36',
+      },
+      cookieJar,
+    });
   }
 
-  async logIn(){
-    console.log(`Logging in using login ${this.login}, password ${this.password} and phone ${this.phoneNumber}`);
+  async logIn() {
+    console.log(
+      `Logging in using login ${this.login}, password ${this.password} and phone ${this.phoneNumber}`
+    );
 
     const preauthResult = await YaAuth.YadPreAuthRequest(this);
     console.log('preauth result: ', preauthResult);
 
-    const loginRequest = await YaAuth.YadAuthLoginRequest(this, preauthResult.csrf, preauthResult.uuid);
+    const loginRequest = await YaAuth.YadAuthLoginRequest(
+      this,
+      preauthResult.csrf,
+      preauthResult.uuid
+    );
 
-    const passwordRequest = await YaAuth.YadAuthPasswordRequest(this, preauthResult.csrf, loginRequest.track_id);
+    const passwordRequest = await YaAuth.YadAuthPasswordRequest(
+      this,
+      preauthResult.csrf,
+      loginRequest.track_id
+    );
 
-    const accountsRequest = await YaAuth.YadAuthAccountsRequest(this, preauthResult.csrf);
+    const accountsRequest = await YaAuth.YadAuthAccountsRequest(
+      this,
+      preauthResult.csrf
+    );
 
     const diskSkResult = await YaAuth.YadAuthDiskSkRequest(this);
 
-    console.log('diskSkResult', diskSkResult)
+    console.log('diskSkResult', diskSkResult);
 
     this.skToken = diskSkResult.skToken;
     this.idClient = diskSkResult.idClient;
@@ -53,22 +67,21 @@ class YandexDiskClient {
     return true;
   }
 
-  public async getFolder(path: string){
+  public async getFolder(path: string) {
     return YaOperations.getFolderInfo(this, path);
   }
 
-  public async getFile(path: string){
+  public async getFile(path: string) {
     return YaOperations.getFileUrl(this, path);
   }
 
-  public async uploadFile(buffer: Buffer, path: string){
+  public async uploadFile(buffer: Buffer, path: string) {
     return YaOperations.uploadFile(this, path, buffer);
   }
 
-  public async createFolder(path: string){
+  public async createFolder(path: string) {
     return YaOperations.createFolder(this, path);
   }
-
 }
 
 export default YandexDiskClient;
