8fa1b9218ab4854abf0b9c7a19d3ce90beb57d59
Pterko <pter96@gmail.com>
2020-08-20 01:54:51 +0300

    Fix test lint

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -2,8 +2,10 @@ import YandexDiskClient from '../src';
 
 describe('blah', () => {
   it('works', () => {
-    const client = new YandexDiskClient(process.env.TEST_LOGIN, process.env.TEST_PASSWORD);
-    
+    const client = new YandexDiskClient(
+      process.env.TEST_LOGIN,
+      process.env.TEST_PASSWORD
+    );
 
     const loginResult = client.logIn();
 
