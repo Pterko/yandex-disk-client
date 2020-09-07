cedf11202da31d5a7c2e9755199b3fdd9785ff87
Pterko <pter96@gmail.com>
2020-09-08 02:27:26 +0300

    Fix linting

--- test/auth.test.ts
+++ test/auth.test.ts
@@ -5,13 +5,10 @@ jest.setTimeout(30000);
 require('dotenv').config();
 
 describe('YandexDriveClient', () => {
-
   it('fails to get client instance if not authenticated', () => {
     expect(() => {
       const authClient = new YandexDiskClientAuth('123', '123');
       authClient.getClientInstance();
-    }).toThrow('Not logged in')
-  })
-
-
+    }).toThrow('Not logged in');
+  });
 });
